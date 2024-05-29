c8a1aa8aa6429a77a4177ce18cb527a18102e515
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-28 14:31:49 -1000

    Update Cook_Book.cmd

--- Cook_Book.cmd
+++ Cook_Book.cmd
@@ -3,17 +3,17 @@ cd /
 cls
 mode con: lines=6 cols=30
 title "Cook Book"
-@cecho off 
+@echo off 
          @123456789012
 @set "apk=145712896014"
 @set "app=951712865014"
 @set "exe=981763543109"
 cecho {04}1=apk{\n}2=app{\n}3=exe{07}
 
-set /a m=Choose;
-if (%m%=1) & goto one
-if (%m%=2) & goto two
-if (%m%=3) & goto three
+set /p m=Choose;
+if (%m%==1) & goto one
+if (%m%==2) & goto two
+if (%m%==3) & goto three
 goto top
 
 :one
